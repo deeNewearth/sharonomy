82d37cb238dba28cdc44ad93f0cd57bb78f3e5ef
shreedee <deepayan@labizbille.com>
2016-09-01 23:58:24 -0400

    react starter

---
+++ site2/src/body.jsx
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+var React = require('react');
+var Grid = require('react-bootstrap').Grid;
+var Row = require('react-bootstrap').Row;
+var Column = require('react-bootstrap').Col;
+
+module.exports =React.createClass({
+    render: function() {
+        return (
+            <Grid>
+                <Row>
+                    <Column md={4}>
+                        col1
+                    </Column>
+                    <Column md={8}>
+                        col3
+                    </Column>
+                </Row>
+            </Grid>
+        );
+    }
+});
---
+++ site2/src/index.jsx
@@ -0,0 +1,12 @@
+﻿var React = require('react');
+var ReactDOM = require('react-dom');
+var Bootstrap = require('bootstrap/dist/css/bootstrap.css');
+var PrimaryContent = require('./Body');
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, jsx! 8</h1>
+    <PrimaryContent />
+  </div>,
+document.getElementById('theApp')
+);
